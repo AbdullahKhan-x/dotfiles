eb1638d4ffc2f64bcffa4cec5e12dc1d24fe4888
AbdullahKhan-x <niazi.khan.abdullah@gmail.com>
2025-08-28 09:13:02 +0500

    First iteration of my system configurations

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a configuration of my personal system
---
+++ home-layout.sh
@@ -0,0 +1,7 @@
+mkdir Archive Dev-Enviornment Games Media
+
+cd Open-Source Personal University System-Configurations
+
+cd ..
+
+cd Media Audio Images Video
